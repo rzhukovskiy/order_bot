ff8e799c89b4bdbd5d4806733169fd6736186dcd
rzhukovskiy <zhukovskey@gmail.com>
2021-09-13 17:55:30 +0200

    pdo set name added

--- src/BaseModel.php
+++ src/BaseModel.php
@@ -26,6 +26,7 @@ class BaseModel
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         ];
         self::$pdo = new PDO($dsn, $user, $password, $opt);
     }
