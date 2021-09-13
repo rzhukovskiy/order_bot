d77ed6387c4327a9189a24e3f94aae8f9eea1bf9
rzhukovskiy <zhukovskey@gmail.com>
2021-09-13 16:51:04 +0200

    main template fixed

--- src/views/templates/main.php
+++ src/views/templates/main.php
@@ -6,7 +6,7 @@
 <!doctype html>
 <html>
 <head>
-    <meta charset="UTF-8" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <title>Orderbot</title>
 </head>
 <body>
